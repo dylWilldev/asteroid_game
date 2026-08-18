e9476d56c268df22d45e9178c80e1a167af41d2c
Dylan <dylanbosch02@gmail.com>
2026-08-17 21:24:24 -0600

    Added logger file to create timestamps and logs for the game when running (ignoring the .jsonl file in the gitignore).  Created constants file to store all constant values for the game and added a game loop in the main function in the main.py file.

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+
 def main():
-    print("Hello from asteroid-game!")
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+    # Game loop
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                # Allows the exit button in the game window to work
+                return 
+
+        screen.fill("black")
+
+        # Refreshes the screen
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
