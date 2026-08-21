5c1e5bd7fb71fb4c2c69a82215dc074e4bcdb407
Dylan <dylanbosch02@gmail.com>
2026-08-20 18:28:35 -0600

    Implemented the ability to render and display the player object on the screen.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -13,6 +14,9 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
 
+    # instantiate player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     # Game loop
     while True:
         log_state()
@@ -23,6 +27,9 @@ def main():
                 return 
 
         screen.fill("black")
+
+        # Render player
+        player.draw(screen) 
 
         # Refreshes the screen
         pygame.display.flip()
